1354b4ca52e0825ef1bfb62ca1f997a34b872e4c
Raheel Mughal <raheelmughal018@gmail.com>
2024-05-13 15:21:29 +0500

    implement left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,25 @@
+import MobileNavbar from "@/components/MobileNavbar";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInUser = { firstName: "Raheel", lastName: "Mughal" };
   return (
-    <main>
-      sidebar
-      {children}
+    <main className="flex w-full font-inter h-screen">
+      <Sidebar user={loggedInUser} />
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src={"/icons/logo.svg"} alt="logo" width={30} height={30} />
+          <div className="">
+            <MobileNavbar user={loggedInUser} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
